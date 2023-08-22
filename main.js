e56da13656f849da27b8a4a0e09669e04af1753f
Selmane Kenzari <Kzselmane@gmail.com>
2023-08-22 07:42:04 +0200

    developement webapp

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-/* effet de défilement fluide avec JavaScript */
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-      e.preventDefault();
+$(document).ready(function() {
+  // Lorsqu'un lien (a) est cliqué
+  $('a[href^="#"]').on('click', function(e) {
+    e.preventDefault();  // Empêche le comportement de navigation par défaut
 
-      const target = document.querySelector(this.getAttribute('href'));
-
-      if (target) {
-          window.scrollTo({
-              top: target.offsetTop - 50, // -50 pour ajuster la position si votre navigation a une hauteur
-              behavior: 'smooth'
-          });
-      }
+    var target = $(this.hash);  // Obtient l'élément cible
+    if (target.length) {
+      // Défile doucement vers l'élément cible en 1000ms (1 seconde)
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);  // Vous pouvez ajuster la durée (1000ms ici) pour rendre le défilement plus rapide ou plus lent
+    }
   });
 });
